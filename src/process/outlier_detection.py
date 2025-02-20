605d0c6ea37663e77204d8b5780b640f4d091685
alextfc <96599449+alextfchan@users.noreply.github.com>
2025-02-20 17:09:29 +0000

    updated outlier_detection to use the new pathreader util

--- src/process/outlier_detection.py
+++ src/process/outlier_detection.py
@@ -1,17 +1,22 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import mean, stddev, col
+from process.utils.pathreader import pathreader
 import pandas as pd
+import yaml
+
+# from utils.pathreader import pathreader #just for testing
 
 
 
-def outlier_detection(spark: SparkSession, complete_data: str) -> dict:
+def outlier_detection(spark: SparkSession, complete_data: dict) -> dict:
     '''
     Function gets data from a CSV file, filters out outliers (focusing on Col: V4), then exports the outliers and remaining data into two parquet files.
     
     Parameters
     ----------
-    complete_data: str
-        Path to the CSV file
+    complete_data: Dict{Dataframe, file paths}
+        DataFrame containing the data from the CSV file, using the pathreader util function
+        File Paths for the output files (source: config.yaml)
 
     Output
     ------
@@ -23,11 +28,11 @@ def outlier_detection(spark: SparkSession, complete_data: str) -> dict:
     Return
     ------
     dict
-        A dictionary containing the outliers and remaining data as DataFrames.
+        - A dictionary containing the outliers and remaining data as DataFrames, as well as the paths to the output files
     '''
 
-    df = spark.read.csv(complete_data, header=True, inferSchema=True) #<- remove
-    # selection = df.select("Time", "V4", "Amount", "Class")
+    df = complete_data["dataframe"]
+    file_path = complete_data["file_path"]
 
     # Remove NULL values
     df.na.drop(subset=["V4"])
@@ -40,21 +45,37 @@ def outlier_detection(spark: SparkSession, complete_data: str) -> dict:
     low = m - (3*s)
 
     # Filter out the outliers for human processing
-    outliers = df.filter((col("V4") >= high) | (col("V4") <= low)) #maybe this should only show column V4.
-    outliers.toPandas().to_parquet("data/anomalies/outliers.parquet") #<- remove
-    # print("Outliers exported to data/anomalies/outliers.parquet")
+    outliers = df.filter((col("V4") >= high) | (col("V4") <= low))
+    outliers_path = file_path["paths"]["anomalies"]+"outliers.parquet"
+    outliers.toPandas().to_parquet(outliers_path)
 
     # Collect remaining data for futher processing
     filtered_data = df.filter((col("V4") < high) & (col("V4") > low))
-    filtered_data.toPandas().to_csv("data/filtered_data.csv") #<- remove
-    # print("Remaining data exported to data/output/remaining.csv")
+    filtered_data_path = file_path["data"]["root"]+"filtered_data.csv"
+    filtered_data.toPandas().to_csv(filtered_data_path)
+
+    # Updating the config file with the new paths -> not sure how to split this one up.
+    # Does this need to be an individual util? Does this even need to happen? I can just simply return the string, rather than doing this process to update the yaml.
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
     
-    
-    return {"outliers": outliers, "remaining": filtered_data}
+    config["data"]["filtered_data_path"] = filtered_data_path
+    config["paths"]["outliers_path"] = outliers_path
+
+    with open("config.yaml", "w") as file:
+        yaml.safe_dump(config, file)
+
+    # returning the paths doesn't help at all. just reveals the path in the return?? But it won't be listed in the repo, just the terminal?
+    return {"outliers": outliers,
+            "outlier_path": outliers_path,
+            "filtered_data": filtered_data,
+            "filtered_path": filtered_data_path,
+            }
 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Outlier Detection").getOrCreate()
-    result = outlier_detection(spark, "data/creditcard.csv")
+    complete_data = pathreader(spark, "complete_data")
+    result = outlier_detection(spark, complete_data)
     spark.stop()
-    print(result)
+  
