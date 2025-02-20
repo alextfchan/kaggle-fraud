fecd626bd9085cd07c073d21f81814e8d523d3a3
alextfc <96599449+alextfchan@users.noreply.github.com>
2025-02-20 17:09:48 +0000

    updated aggregate to use the new pathread util

--- src/process/aggregate.py
+++ src/process/aggregate.py
@@ -1,8 +1,13 @@
 from pyspark.sql import SparkSession, DataFrame
+from process.utils.pathreader import pathreader
 import pandas as pd
+import yaml
+
+# from utils.pathreader import pathreader #just for testing
+# from outlier_detection import outlier_detection  #just for testing
 
 
-def aggregate(spark: SparkSession, filtered_data: str) -> DataFrame:
+def aggregate(spark: SparkSession, filtered_data: DataFrame, file_path: dict) -> dict:
     '''
     Function gets data from a CSV file, aggregates it, then exports the results in a parquet file.
 
@@ -10,33 +15,56 @@ def aggregate(spark: SparkSession, filtered_data: str) -> DataFrame:
     ----------
     spark: SparkSession
         spark session required to run and process the data
-    complete_data: str
-        path to the CSV file
+    filtered_data: DataFrame
+        DataFrame containing the filtered data
+    file_path: dict
+        File Paths for the output files (source: config.yaml)
 
+        
     Output
     ------
     Parquet file:
         aggregated data from the CSV file
-
+    Updates the config file with the new paths in config.yaml
+        
     Return
     ------
-    summary: DataFrame
-        summary of the aggregated data
+    dict
+        summary of the aggregated data as a DataFrame, and the path to the output file
     '''
 
-    df = spark.read.csv(filtered_data, header=True, inferSchema=True)
-    summary = df.describe()
+    aggregate_path = file_path["file_path"]["paths"]["anomalies"]+"aggregate.parquet"
+
+    summary = filtered_data.describe()
 
     export = summary.toPandas()
+    export.to_parquet(aggregate_path)
+
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+    
+    config["paths"]["aggregate_path"] = aggregate_path
+
+    with open("config.yaml", "w") as file:
+        yaml.safe_dump(config, file)
+
 
     # export.to_csv("data/output/aggregate.csv")
-    export.to_parquet("data/output/aggregate.parquet")
+    # export.to_parquet("data/output/aggregate.parquet")
         
-    return summary
+    return {"summary": summary, "aggregate_path": aggregate_path}
 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Aggregate").getOrCreate()
-    result = aggregate(spark, "data/filtered_data.csv")
+
+    #pipeline
+    complete_data = pathreader(spark, "complete_data")
+    filtered_data = outlier_detection(spark, complete_data)
+
+    #actual function
+    result = aggregate(spark,
+                       filtered_data["filtered_data"],
+                       complete_data)
     spark.stop()
     print(result)
