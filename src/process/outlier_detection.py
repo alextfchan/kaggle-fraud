557fdf5f5387c340bb6759113fb27ef60cbca329
alextfc <96599449+alextfchan@users.noreply.github.com>
2025-04-01 16:12:06 +0100

    migration: pyspark to polars, updated outlier_detection and related test

--- src/process/outlier_detection.py
+++ src/process/outlier_detection.py
@@ -1,18 +1,16 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import mean, stddev, col
+from polars import col
 import logging
 from src.process.utils.pathreader import pathreader
 from src.process.utils.exporter import to_parquet, to_csv
-import yaml
 
 
 logger = logging.getLogger()
 
 
-def outlier_detection(spark: SparkSession, complete_data: dict) -> dict:
-    '''
+def outlier_detection(complete_data: dict) -> dict:
+    """
     Function gets data from a CSV file, filters out outliers (focusing on Col: V4), then exports the outliers and remaining data into two parquet files.
-    
+
     Parameters
     ----------
     complete_data: Dict{Dataframe, file paths}
@@ -25,12 +23,12 @@ def outlier_detection(spark: SparkSession, complete_data: dict) -> dict:
         Data with V4 values filtered for outliers for human processing
     remaining.csv: CSV file
         Data with V4 values filtered for non-outliers for further processing
-    
+
     Return
     ------
     dict
         - A dictionary containing the outliers and remaining data as DataFrames, as well as the paths to the output files
-    '''
+    """
 
     outlier_data = None
     filtered_data = None
@@ -40,37 +38,40 @@ def outlier_detection(spark: SparkSession, complete_data: dict) -> dict:
         file_path = complete_data["file_path"]
 
         # Remove NULL values
-        df.na.drop(subset=["V4"])
+        df.drop_nulls(subset="V4")
 
         # Remove extreme outliers (3 standard deviations from the mean)
-        m = df.agg(mean("V4")).collect()[0][0]
-        s = df.agg(stddev("V4")).collect()[0][0]
+        m = df["V4"].mean()
+        s = df["V4"].std()
 
-        high = m + (3*s)
-        low = m - (3*s)
+        high = m + (3 * s)
+        low = m - (3 * s)
 
-        # Filter out the outliers for human processing
+        # # Filter out the outliers for human processing
         outlier_data = df.filter((col("V4") >= high) | (col("V4") <= low))
-        outliers_path = file_path["paths"]["anomalies"]+"outliers.parquet"
+        outliers_path = file_path["paths"]["anomalies"] + "outliers.parquet"
         to_parquet(outlier_data, outliers_path)
 
-        # Collect remaining data for futher processing
+        # # Collect remaining data for futher processing
         filtered_data = df.filter((col("V4") < high) & (col("V4") > low))
-        filtered_data_path = file_path["data"]["root"]+"filtered_data.csv"
+        filtered_data_path = file_path["data"]["root"] + "filtered_data.csv"
         to_csv(filtered_data, filtered_data_path)
-        
+
+    except TypeError as te:
+        logger.exception(f"TypeError reading file: {te}")
+    except AttributeError as ae:
+        logger.exception(f"AttributeError reading file: {ae}")
+    except KeyError as ke:
+        logger.exception(f"KeyError reading dict from pathreader function: {ke}")
     except Exception as e:
         logger.exception(f"Error in Outlier Detection: {e}")
 
     logger.info("Outlier Dectection has been completed.")
 
-    return {"outliers_data": outlier_data,
-            "filtered_data": filtered_data}
+    return {"outliers_data": outlier_data, "filtered_data": filtered_data}
 
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Outlier Detection").getOrCreate()
-    complete_data = pathreader(spark, "config.yaml", "complete_data")
-    result = outlier_detection(spark, complete_data)
-    spark.stop()
-  
+    complete_data = pathreader("config.yaml", "complete_data")
+    result = outlier_detection(complete_data)
+    print("actual: ", result)
--- tests/test_outlier_detection.py
+++ tests/test_outlier_detection.py
@@ -1,126 +1,50 @@
 import pytest
-from pyspark.sql import SparkSession, DataFrame
+from polars import DataFrame
 from src.process.outlier_detection import outlier_detection
-import logging
-import yaml
+from .test_data import sample_data as data
 
-logger = logging.getLogger()
-
-expected_columns = [
-    "Time",
-    "V1",
-    "V2",
-    "V3",
-    "V4",
-    "V5",
-    "V6",
-    "V7",
-    "V8",
-    "V9",
-    "V10",
-    "V11",
-    "V12",
-    "V13", 
-    "V14",
-    "V15",
-    "V16",
-    "V17",
-    "V18",
-    "V19",
-    "V20",
-    "V21",
-    "V22",
-    "V23",
-    "V24",
-    "V25",
-    "V26",
-    "V27",
-    "V28",
-    "Amount",
-    "Class",
-]
 
 key = ["outliers_data", "filtered_data"]
 
 
-@pytest.fixture(scope="session")
-def spark():
-    spark = SparkSession.builder.appName("TestOutlierDetected").getOrCreate()
-    yield spark
-    spark.stop()
-
-@pytest.fixture(scope="function")
-def test_data(tmp_path):
-    data = """Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount,Class
-    0,-1,-0.1,2.5,1.3,-0.3,0.4,0.2,0.09,0.3,0.2,-0.5,-0.6,-0.9,-0.3,1.6,-0.4,0.2,0.025,0.4,0.25,-0.04,0.27,-0.1,0.066,0.1,-0.17,0.1,-0.02,149.62,0
-    1,1.19,0.2,0.16,0.44,0.06,-0.082,-0.078,0.0851,-0.2,-0.16,1.61,1.06,0.48,-0.14,0.63,0.4,-0.11,-0.1,-0.14,-0.0,-0.22,0.3,-0.49,0.0,0.24,-0.0,0.11,-0.0,2.69,0"""
-    data_path = tmp_path / "test_creditcard.csv"
-    data_path.write_text(data)
-    return str(data_path)
-
-@pytest.fixture(scope="function")
-def test_config(test_data, tmp_path):
-    '''
-    Creating a temporary config file so that the original config.yaml is not modified during the process.
-    '''
-    config_content = f"""
-    data:
-        complete_data: {test_data}
-        root: {str(tmp_path)}
-    paths:
-        anomalies: {str(tmp_path)}
-        output: {str(tmp_path)}
-    """
-    config_path = tmp_path / "test_config.yaml"
-    config_path.write_text(config_content)
-    return str(config_path)
-
-@pytest.fixture(scope="function")
-def test_complete_data(spark, test_data, test_config):
-    '''
-    Test dictionary which would have been received from pathreader.py function.
-    '''
-    with open(test_config, "r") as f:
-        config_content = yaml.safe_load(f)
-
-    test_complete_data = {
-        "dataframe": spark.read.csv(test_data, header=True, inferSchema=True),
-        "config_file": test_config,
-        "file_path": config_content,
-        }
-    return test_complete_data
+@pytest.mark.parametrize("data, expected", [(data[5], DataFrame)])
+def test_output_is_dataframe(data, expected, test_complete_data):
+    actual = outlier_detection(test_complete_data)
+    # print(actual)
+    for df in actual:
+        assert isinstance(actual[df], expected)
 
 
-def test_output_is_dataframe(spark, test_complete_data):
-    actual = outlier_detection(spark, test_complete_data)
+@pytest.mark.parametrize("data, expected", [(data[5], 2)])
+def test_number_of_keys_returned(data, expected, test_complete_data):
+    actual = outlier_detection(test_complete_data)
+    assert len(actual) == expected
+
+
+@pytest.mark.parametrize("data, expected", [(data[5], "full_expected_columns")])
+def test_column_output(data, expected, test_complete_data, request):
+    expected = request.getfixturevalue("full_expected_columns")
+    actual = outlier_detection(test_complete_data)
     for df in key:
-        assert isinstance(actual[df], DataFrame)
-    
-def test_number_of_keys_returned(spark, test_complete_data):
-    actual = outlier_detection(spark, test_complete_data)
-    assert len(actual) == 2
+        assert actual[df].columns == expected
 
-def test_column_output(spark, test_complete_data):
-    actual = outlier_detection(spark, test_complete_data)
-    for df in key:
-        assert actual[df].columns == expected_columns
 
-def test_output_returns_none_if_no_df(spark):
+def test_output_returns_none_if_no_df():
     test_bad_data = None
-    actual = outlier_detection(spark, test_bad_data)
+    actual = outlier_detection(test_bad_data)
     for df in key:
         assert actual[df] is None
 
-def test_missing_complete_data(spark):
-    bad_data = {"dataframe": None,
-            "config_file": "",
-            "file_path": {}}
-    actual = outlier_detection(spark, bad_data)
+
+def test_missing_complete_data():
+    bad_data = {"dataframe": None, "config_file": "", "file_path": {}}
+    actual = outlier_detection(bad_data)
     expected = {"outliers_data": None, "filtered_data": None}
     assert actual == expected
 
-def test_incorrect_dict_input(spark):
+
+def test_incorrect_dict_input():
     bad_data = {}
-    actual = outlier_detection(spark, bad_data)
+    actual = outlier_detection(bad_data)
     expected = {"outliers_data": None, "filtered_data": None}
     assert actual == expected
