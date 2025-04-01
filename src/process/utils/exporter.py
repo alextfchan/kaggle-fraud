6441285752951807d934bd26c6421f4b25304145
alextfc <96599449+alextfchan@users.noreply.github.com>
2025-04-01 15:46:48 +0100

    migration: pyspark to polars, exporter util

--- src/process/utils/exporter.py
+++ src/process/utils/exporter.py
@@ -1,28 +1,27 @@
-import pandas as pd
+from polars import DataFrame
 import logging
 
 
 logger = logging.getLogger()
 
 
-def to_parquet(df: pd.DataFrame, file_path: str) -> None:
-    '''
+def to_parquet(df: DataFrame, file_path: str) -> None:
+    """
     Function exports a DataFrame to a Parquet file.
-    '''
+    """
     try:
-        df.toPandas().to_parquet(file_path)
-        logger.info(f"Successfully exported to Parquet: {file_path}")
+        df.write_parquet(file_path)
+        logger.info(f"Data: {df} successfully exported to Parquet: {file_path}")
     except Exception as e:
         logger.exception(f"Error exporting to Parquet: {e}")
 
 
-def to_csv(df: pd.DataFrame, file_path: str) -> None:
-    '''
+def to_csv(df: DataFrame, file_path: str) -> None:
+    """
     Function exports a DataFrame to a CSV file.
-    '''
+    """
     try:
-        df.toPandas().to_csv(file_path)
-        logger.info(f"Successfully exported to CSV: {file_path}")
-
+        df.write_csv(file_path)
+        logger.info(f"Data: {df} successfully exported to CSV: {file_path}")
     except Exception as e:
         logger.exception(f"Error exporting to CSV: {e}")
