5959b2ffe42c66f98b4734bfedd3ab730988489c
alextfc <96599449+alextfchan@users.noreply.github.com>
2025-04-02 13:34:45 +0100

    updated main pipeline function to work with polars

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,13 +1,14 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.utils import AnalysisException
-import pandas as pd
-from process.utils.pathreader import pathreader
+import polars as pl
+from src.process.utils.pathreader import pathreader
 from src.process.outlier_detection import outlier_detection
 from src.process.aggregate import aggregate
 from src.process.analysis import analysis
 
-def pipeline(spark: SparkSession, complete_data: dict) -> dict:
-    '''
+# logger = logging.getLogger()
+
+
+def pipeline(config: str, file: str = "complete_data") -> dict:
+    """
     Function that runs the entire pipeline.  The pipeline consists of the following steps:
     1. Outlier Detection
     2. Analysis
@@ -15,10 +16,11 @@ def pipeline(spark: SparkSession, complete_data: dict) -> dict:
 
     Parameters
     ----------
-    spark: SparkSession
-        spark session required to run and process the data
-    complete_data: str
-        uses the util function 'pathreader' to read the CSV file
+    config: str
+        name of the config.yaml file
+    file: str
+        default value: "complete_data"
+        name of the file to read within config.yaml
 
     Output
     ------
@@ -34,34 +36,24 @@ def pipeline(spark: SparkSession, complete_data: dict) -> dict:
     ------
     dict
         a dictionary containing the results in DataFrame format from the outlier detection, analysis, and aggregation processes
-    '''
-    try:
-        # Step 1: Reading the CSV file
-        # data = pathreader(spark, complete_data)
+    """
+    # Step 1: Reading the CSV file <-should be able to run just from the config. E&O = io, T should be decoupled.
+    data = pathreader(config, file)
 
-        # Step 1: Outlier Detection
-        outlier_result = outlier_detection(spark,
-                                           complete_data)
-        # Step 2: Analysis
-        analysis_result = analysis(spark,
-                                   outlier_result["filtered_data"],
-                                   complete_data)
-        # Step 3: Aggregate
-        aggregate_result = aggregate(spark,
-                                     outlier_result["filtered_data"],
-                                     complete_data)
-    except AnalysisException as ae:
-        print("Analysis Exception: ", ae) # normally for path not exist?
-        raise ae
-    except Exception as e:
-        # return {"error": fnf}
-        print("Error: ", e)
-    # except FileNotFoundError as fnf:
-    #     print("File not found: ", fnf)
-    return {"outlier_result": outlier_result, "analysis_result": analysis_result, "aggregate_result": aggregate_result}
+    # Step 2: Outlier Detection
+    outlier_result = outlier_detection(data)
+    # Step 3: Analysis
+    analysis_result = analysis(outlier_result["filtered_data"], data)
+    # Step 4: Aggregate
+    aggregate_result = aggregate(outlier_result["filtered_data"], data)
+
+    return {
+        "outlier_result": outlier_result,
+        "analysis_result": analysis_result,
+        "aggregate_result": aggregate_result,
+    }
+
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Pipeline").getOrCreate()
-    complete_data = pathreader(spark, "complete_data") # should this be in the pipeline?
-    result = pipeline(spark, complete_data)
-    spark.stop()
+    result = pipeline("config.yaml")
+    print(result)
